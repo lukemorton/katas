035a18cb4f217616a788ffdfd29b6c6167c5f62c
Luke Morton <lukemorton.dev@gmail.com>
2019-02-02 12:36:59 +0000

    Generalised solution

--- spec/convert_integer_to_roman_numeral_spec.rb
+++ spec/convert_integer_to_roman_numeral_spec.rb
@@ -1,22 +1,30 @@
 class ConvertIntegerToRomanNumeral
   def execute(integer)
-    return x(integer / 10) if [20, 30, 40].include?(integer)
+    roman_numeral = ''
 
-    if integer.between?(1, 3)
-      i(integer)
-    elsif integer.between?(5, 8)
-      'V' + i(integer - 5)
-    elsif integer.between?(10, 13)
-      'X' + i(integer - 10)
-    elsif integer.between?(15, 18)
-      'XV' + i(integer - 15)
+    if integer >= 10
+      roman_numeral << x(integer / 10)
+      integer = integer % 10
     end
+
+    if integer >= 5
+      roman_numeral << v(1)
+      integer = integer - 5
+    end
+
+    roman_numeral << i(integer)
+
+    roman_numeral
   end
 
   private
 
   def i(integer)
     'I' * integer
+  end
+
+  def v(integer)
+    'V' * integer
   end
 
   def x(integer)
@@ -42,8 +50,29 @@ RSpec.describe ConvertIntegerToRomanNumeral do
     17 => 'XVII',
     18 => 'XVIII',
     20 => 'XX',
+    21 => 'XXI',
+    22 => 'XXII',
+    23 => 'XXIII',
+    25 => 'XXV',
+    26 => 'XXVI',
+    27 => 'XXVII',
+    28 => 'XXVIII',
     30 => 'XXX',
+    31 => 'XXXI',
+    32 => 'XXXII',
+    33 => 'XXXIII',
+    35 => 'XXXV',
+    36 => 'XXXVI',
+    37 => 'XXXVII',
+    38 => 'XXXVIII',
     40 => 'XXXX',
+    41 => 'XXXXI',
+    42 => 'XXXXII',
+    43 => 'XXXXIII',
+    45 => 'XXXXV',
+    46 => 'XXXXVI',
+    47 => 'XXXXVII',
+    48 => 'XXXXVIII',
   }
 
   expectations.each do |(integer, roman_numeral)|
