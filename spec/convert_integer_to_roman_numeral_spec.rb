3ebb8bba7d6e197a304a8af522e0f52c768f0999
Luke Morton <lukemorton.dev@gmail.com>
2019-02-02 12:43:11 +0000

    Move production code to lib

--- spec/convert_integer_to_roman_numeral_spec.rb
+++ spec/convert_integer_to_roman_numeral_spec.rb
@@ -1,44 +1,4 @@
-class ConvertIntegerToRomanNumeral
-  def execute(integer)
-    roman_numeral = ''
-
-    if integer >= 10
-      roman_numeral << x(integer / 10)
-      integer = integer % 10
-    end
-
-    if integer == 9
-      roman_numeral << i(1) + x(1)
-      integer = integer - 9
-    end
-
-    if integer >= 5
-      roman_numeral << v(1)
-      integer = integer - 5
-    end
-
-    if integer == 4
-      roman_numeral << i(1) + v(1)
-      integer = integer - 4
-    end
-
-    roman_numeral << i(integer)
-  end
-
-  private
-
-  def i(integer)
-    'I' * integer
-  end
-
-  def v(integer)
-    'V' * integer
-  end
-
-  def x(integer)
-    'X' * integer
-  end
-end
+require_relative '../lib/convert_integer_to_roman_numeral'
 
 RSpec.describe ConvertIntegerToRomanNumeral do
   expectations = {
